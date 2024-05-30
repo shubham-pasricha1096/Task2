e71f09f02c749882e823df5526e38f1bd80b956a
shubham-pasricha1096 <120403393+shubham-pasricha1096@users.noreply.github.com>
2024-05-30 15:34:28 +0530

    Add files via upload

---
+++ data.py
@@ -0,0 +1,31 @@
+import pandas as pd
+data = pd.read_csv('C:\\C tutorials\\Internship\\task 2\\01.Data Cleaning and Preprocessing.csv')
+print(type(data))
+print(data.info)
+print(data.shape)
+print(data.describe())
+data = data.drop_duplicates()
+print(data)
+print(data.isnull())
+print(data.isnull().sum())
+print(data.notnull())
+print(data.isnull().sum().sum())
+data2 = data.fillna(value=7)
+print(data2)
+print(data2.isnull().sum().sum())
+# backward filling
+data3 = data.fillna(method='pad') 
+print(data3)
+# forward filing  
+data4 = data.fillna(method='bfill')
+print(data4)
+print(data2.columns)
+data2.drop(['Observation'],axis=1,inplace=True)
+print(data2.columns)
+Q1 = data2.quantile(0.25)
+Q3 = data2.quantile(0.75)
+IQR = Q3-Q1
+print(IQR)
+data2=data2[~((data2<(Q1-1.5*IQR))|(data2>(Q3+1.5*IQR))).any(axis=1)]
+print(data2)
+print(data2.describe())
